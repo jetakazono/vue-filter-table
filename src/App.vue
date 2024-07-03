8e4af8dcb75e65b42382f33522672ebe71328a62
jetakazono <jessica.takazono@gmail.com>
2024-07-03 23:01:14 +0100

    first commit: add table

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import DataTable from './components/DataTable.vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  const response = await fetch('https://dummyjson.com/todos')
+  const data = await response.json()
+  items.value = data.todos
+})
+</script>
+
+<template>
+  <div class="p-8 bg-gray-100 min-h-screen">
+    <DataTable :items="items" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="bg-white relative border rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-300">
+        <tr>
+          <th class="px-4 py-3">ID</th>
+          <th class="px-4 py-3">Assigned To</th>
+          <th class="px-4 py-3">Status</th>
+          <!-- <th class="px-4 py-3">Title</th> -->
+          <!-- <th class="px-4 py-3">Due At</th> -->
+          <th class="px-4 py-3"><span class="sr-only">Actions</span></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id" class="border-b">
+          <td class="px-4 py-3 font-medium text-gray-900">{{ item.id }}</td>
+          <td class="px-4 py-3 font-medium text-gray-900">{{ item.todo }}</td>
+          <td class="px-4 py-3">{{ item.completed }}</td>
+          <td class="px-4 py-3 flex items-center justify-end">
+            <a class="text-indigo-500 hover:underline" href="#">Details</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
+
+const props = defineProps({ items: { type: Array, required: true } })
